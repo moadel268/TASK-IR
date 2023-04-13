51e05fee44357dc6bf9e45dd130bab59a9cee741
moadel268 <54047634+moadel268@users.noreply.github.com>
2023-04-13 11:49:11 +0200

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,28 @@
+import re
+def remove_stop_words(text, stop_words):
+    words = text.split()
+    filtered_text = " ".join([word for word in words if word.lower() not in stop_words])
+    return filtered_text
+documents = {
+    1: "new home sales top forecasts",
+    2: "home sales rise in July",
+    3: "increase in home sales in July",
+    4: "July new home sales rise",
+}
+stop_words = {"in", "is", "a", "and", "are"}
+for doc_id, text in documents.items():
+    documents[doc_id] = remove_stop_words(text, stop_words)
+inverted_index = {}
+
+for doc_id, text in documents.items():
+    words = set(re.findall(r'\w+', text.lower()))
+    for word in words:
+        if word in inverted_index:
+            inverted_index[word].add(doc_id)
+        else:
+            inverted_index[word] = {doc_id}
+search_term = input("Enter a word: ").strip().lower()
+if search_term in inverted_index:
+    print("Output:", ",".join(map(str, sorted(inverted_index[search_term]))))
+else:
+    print("Output: No matching documents found.")
